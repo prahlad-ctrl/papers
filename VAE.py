748f94388888e379d1b7708ff80942c92dc26fdb
Prahlad Alaparthi <alaparthi.prahlad@gmail.com>
2026-02-03 16:30:35 +0530

    vae

--- VAE.py
+++ VAE.py
@@ -7,8 +7,6 @@ from torchvision import transforms
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 
-from model import VariationalAutoEncoder
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_dim = 784
 h_dim = 200
@@ -17,6 +15,36 @@ epochs = 10
 batch_size = 32
 lr = 1e-3
 
+# input img -> hidden dim -> mean, std -> parameterization -> decoder -> output img
+class VariationalAutoEncoder(nn.Module):
+    def __init__(self, input_dim, h_dim = 200, z_dim = 20): # latent space dim
+        super().__init__()
+        # encoder
+        self.img_2hid = nn.Linear(input_dim, h_dim)
+        self.hid_2mu = nn.Linear(h_dim, z_dim)
+        self.hid_2sigma = nn.Linear(h_dim, z_dim)
+        # decoder
+        self.z_2hid = nn.Linear(z_dim, h_dim)
+        self.hid_2img = nn.Linear(h_dim, input_dim)
+        
+        self.relu = nn.ReLU()
+        
+    def encode(self, x): # q_phi(z/x)
+        h = self.relu(self.img_2hid(x))
+        mu, sigma = self.hid_2mu(h), self.hid_2sigma(h) # z ~ N(mu, sigma)
+        return mu, sigma
+    
+    def decode(self, z): # q_theta(x/z)
+        h = self.relu(self.z_2hid(z))
+        return torch.sigmoid(self.hid_2img(h))
+    
+    def forward(self, x):
+        mu, sigma = self.encode(x)
+        epsilon = torch.randn_like(sigma)
+        z_new = mu + sigma* epsilon # reparameterization
+        x_reconstructed = self.decode(z_new)
+        return x_reconstructed, mu, sigma
+       
 dataset = datasets.MNIST(root = 'dataset/', train = True, transform=transforms.ToTensor(), download= True)
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle= True)
 model = VariationalAutoEncoder(input_dim, h_dim, z_dim).to(device)
@@ -66,7 +94,7 @@ def inference(digit, num_ex = 1):
         z = mu + sigma* epsilon
         out = model.decode(z)
         out = out.view(-1, 1, 28, 28)
-        save_image(out, f'{digit}_ex{ex}.png')
+        save_image(out, f'{digit}.png')
         
 for i in range(10):
     inference(i, num_ex= 1)
